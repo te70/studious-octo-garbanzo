00bc567ef0071da7f10db76857c93a326db51dae
te70 <tevinkinuthia@gmail.com>
2023-10-10 19:13:17 +0400

    Add files

---
+++ src/api/todos/todos.route.ts
@@ -0,0 +1,14 @@
+import { Router, Response, Request } from 'express';
+import Todo from './todos.model';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response<Todo[]>) => {
+  res.json([{
+    content: 'Learn Ts',
+    done: false,
+  }]);
+});
+
+export default router;
+
